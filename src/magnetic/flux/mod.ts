1169859686a3bd05c453abac73975684f86650e7
Bernd Amend <typescript@berndamend.de>
2025-11-19 00:52:24 +0100

    chore(magnetic/flux/mod.ts): add missing /mod.ts to import

--- src/magnetic/flux/mod.ts
+++ src/magnetic/flux/mod.ts
@@ -1,7 +1,7 @@
 import type * as dimension from "./dimension.ts";
 import { type Arithmetic, NativeArithmetic } from "../../arithmetic.ts";
 import type { Quantity, Unit } from "../../unit.ts";
-import { withValueType as currentWithValueType } from "../../electric/current";
+import { withValueType as currentWithValueType } from "../../electric/current/mod.ts";
 import { withValueType as lengthWithValueType } from "../../length/mod.ts";
 import { withValueType as massWithValueType } from "../../mass/mod.ts";
 import { withValueType as timeWithValueType } from "../../time/mod.ts";
