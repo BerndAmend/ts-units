7b27e913534f29a8564cbba0580ab06a56ca0dce
Bernd Amend <typescript@berndamend.de>
2025-11-19 23:48:46 +0100

    chore: re-enable the benchmarks

---
+++ test/benchmark_bench.ts
@@ -0,0 +1,22 @@
+import { makeUnit } from "../src/unit.ts";
+
+Deno.bench("makeUnit", () => {
+  makeUnit("m", { length: 1 });
+});
+
+Deno.bench("makeQuantity", () => {
+  meters(20);
+});
+
+const meters = makeUnit("m", { length: 1 });
+const length1 = meters(20);
+
+const yards = meters.times(0.9144);
+const length2 = yards(13);
+Deno.bench("plus", () => {
+  length1.plus(length2);
+});
+
+Deno.bench("plus without ts-units", () => {
+  20 + 11.8872;
+});
